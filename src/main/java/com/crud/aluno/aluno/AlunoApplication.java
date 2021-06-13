fb3fa39449a443e6634167dec6f76b96cdc9f5ce
Jeferson Pereira da Silva <jeferson_p.silva@outlook.com>
2021-06-13 10:50:44 -0300

    *Criado estrutura inicial do projeto com Spring IO

---
+++ src/main/java/com/crud/aluno/aluno/AlunoApplication.java
@@ -0,0 +1,13 @@
+package com.crud.aluno.aluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoApplication.class, args);
+	}
+
+}
